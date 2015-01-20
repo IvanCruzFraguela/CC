d796ca98beeac4c078b247f4d92f96ab5ee17129
Ibán Cruz <ivancruzfraguela@gmail.com>
2015-01-20 15:06:08 +0100

    para ver si funciona a la primera ahora

--- LibCC/Class1.cs
+++ LibCC/Class1.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace LibCC
 {
+    public interface IElementoDisco
+    {
+        public string Nombre;
+        public CDirectorio Padre;
+        public string NombreCompleto;
+        public bool Existe;
+    }
     public class CDirectorio
     {
+
     }
     public class CFichero
     {
